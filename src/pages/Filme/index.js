7718ebf4646dda62d9b331c9e582d56eb8b1a54e
Décio <deciocavalcantedeandrade@gmail.com>
2023-07-24 21:36:24 -0300

    Detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,64 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import './filme-info.css';
+import api from '../../services/api';
 
 function Filme(){
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: "28fc232cc001c31e8a031f419d0a14ca",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(()=>{
+        console.log("FILME NAO ENCONTRADO")
+      })
+    }
+
+    loadFilme();
+
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO")
+    }
+  }, [])
+
+  if(loading){
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+  
   return(
-    <div>
-      <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avalição: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
+
     </div>
   )
 }
