f2bae6427a17c04f1b46b4037df3baea14e4e0a7
Décio <deciocavalcantedeandrade@gmail.com>
2023-07-24 22:51:05 -0300

    Manipulando lista de filmes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -12,9 +12,21 @@ function Favoritos(){
 
   }, [])
 
+
+  function excluirFilme(id){
+    let filtroFilmes = filmes.filter( (item) => {
+      return (item.id !== id)
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes) )
+  }
+
   return(
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :( </span>}
 
       <ul>
         {filmes.map((item) => {
@@ -24,7 +36,7 @@ function Favoritos(){
 
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id) }>Excluir</button>
               </div>
             </li>
           )
