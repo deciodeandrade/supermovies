f3a4b411646e70a307a7436c52a61c52b2c2c217
Décio <deciocavalcantedeandrade@gmail.com>
2023-07-29 18:09:10 -0300

    Adicionando footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="columns bigger-footer">
+      </div>
+      <div className="columns is-mobile smaller-footer" >
+        <div className="column content has-text-centered">
+          <p>
+            <span className="copyright">
+              © {new Date().getFullYear()} - Super Movies
+            </span>
+            <br />
+            <span className="desenvolvedor">
+              Desenvolvido por <a className="desenvolvedor" href="https://www.linkedin.com/in/d%C3%A9cio-de-andrade-3481b91a2/" target="_blank">Décio de Andrade</a>
+            </span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
